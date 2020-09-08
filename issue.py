56a65b57607afa7219cfee370e886fc48840f858
pat <pat.li@dell.com>
2020-09-08 10:39:45 +0800

    add workflow yml

--- issue.py
+++ issue.py
@@ -1,12 +1,12 @@
-import requests
+import os
 
-import tokens
+import requests
 
 
 def create_issue(title, body, milestone=None, labels=None, assignees=None):
     url = 'https://api.github.com/repos/xueyuanl/daily-hackernews/issues'
 
-    headers = {'Content-Type': 'application/json', 'Authorization': 'token ' + tokens.token}
+    headers = {'Content-Type': 'application/json', 'Authorization': 'token ' + os.getenv('ACCESS_TOKEN')}
     body = {'title': title, 'body': body}
     result = requests.post(url=url, json=body, headers=headers)
 
